17538ff48271f6f54fe47b7ffcd09b0dd186c8d7
Mridul Vasudeva <mridulvasudeva.03@gmail.com>
2024-09-02 01:21:20 +0530

    trying

--- src/app/raise-ticket/page.jsx
+++ src/app/raise-ticket/page.jsx
@@ -43,7 +43,7 @@ export default function TicketManager() {
     event.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sendEmail`, {
+      const response = await fetch(`/sendEmail`, {
         method: "POST",
         headers: {
           "Content-type": "application/json",
